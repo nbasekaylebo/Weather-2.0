d6c97a5fd0b47724da1b8a5b13e5d0dda5660da5
nbasekaylebo <nannalebo@gmail.com>
2021-05-23 01:56:02 +0100

    footer and weather icons

--- script-app.js
+++ script-app.js
@@ -35,6 +35,12 @@ function displayWeatherInfo(response) {
     response.data.wind.speed
   );
 
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
 
